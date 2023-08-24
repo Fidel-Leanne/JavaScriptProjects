c90f4268fc699434cd1940c566e71a7f697753e5
fidelia <fidelialtm13@gmail.com>
2023-08-24 19:57:22 +0200

    JS18

---
+++ nodeJs/http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+         res.end('welcome to our home page');
+    }
+
+    if (req.url === '/about') {
+         res.end('here is our short history');
+    }
+
+    res.end("<h1 style='color:red'>Oops</h1></body>");
+});
+
+server.listen(5000);
