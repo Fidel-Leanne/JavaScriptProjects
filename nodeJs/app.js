aa0b68770943fd6002b2c608e843f60393c5c1cb
fidelia <fidelialtm13@gmail.com>
2023-08-27 18:45:17 +0200

    JS23

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -1,34 +1,39 @@
-const {readFile, writeFile}= require('fs')
+const { readFile, writeFile } = require('fs');
+const http = require('http');
 
-readFile('./content/first.txt', 'utf8', (err, result)=>{
-
-    if(err){
-        console.log("Error", err)  // Error: ENOENT: no such file or directory, open './content/first.txt'
-        return 
+// Create a new file
+const filename = './content/first.txt';
+writeFile(filename, 'hello its fidel', err => {
+    if (err) {
+        console.error('Error writing to file:', err);
+        return;
     }
-  console.log(result)  
-    
-})
+    console.log(`File ${filename} has been created!`);
+});
 
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf-8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
 
-writeFile("./content/second.txt",
-"Hello World! its fidel", "utf-8" ,(err,result )=>{
-  if (err){
-    console.log("error", err)
-  }
-  console.log(result)
-  } );
+const server = http.createServer(async (req, res) => {
+    console.log('request event');
+    try {
+        const text = await getText('./content/second.txt');
+        res.end(text);
+    } catch (err) {
+        res.end('Error reading file.');
+    }
+});
 
-
-readFile('./content/second.txt', 
-    'utf8',(err, result)=>{
-      if(err ){
-        console.log("No content")
-        return
-        }
-          console.log(`Content is ${result}`)
-          
-    } )
- console.log('HEYY PEOPLE')
-
-
+const port = 5000;
+server.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+});
