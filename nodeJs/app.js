9902bef7f2e764fa839aa3c08eedf8a572879f27
fidelia <fidelialtm13@gmail.com>
2023-08-28 16:00:47 +0200

    JS25

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -1,33 +1,46 @@
 const { readFile, writeFile } = require('fs');
 const http = require('http');
+const util= require('util')
+
+const readFilePromise= util.promisify(readFile)
+const writeFilePromise= util.promisify(writeFile)
 
 // Create a new file
 const filename = './content/first.txt';
-writeFile(filename, 'hello its fidel', err => {
-    if (err) {
-        console.error('Error writing to file:', err);
-        return;
-    }
-    console.log(`File ${filename} has been created!`);
-});
+// writeFile(filename, 'hello its fidel', err => {
+//     if (err) {
+//         console.error('Error writing to file:', err);
+//         return;
+//     }
+//     console.log(`File ${filename} has been created!`);
+// });
 
-const getText = (path) => {
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf-8', (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
-    });
-};
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf-8', (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(data);
+//             }
+//         });
+//     });
+// };
+
+
+// getText('./content/first.txt')
+// .then ((result)=>console.log(result))
+// .catch((err)=>console.log(err))
 
 const server = http.createServer(async (req, res) => {
     console.log('request event');
     try {
-        const text = await getText('./content/second.txt');
-        res.end(text);
+       const first= await readFilePromise('./content/first.txt','utf-8' )
+       const second=await readFilePromise('./content/second.txt','utf-8')
+
+       console.log(first)
+       res.end(`${first} ${second}`)
+        
     } catch (err) {
         res.end('Error reading file.');
     }
--- nodeJs/globals.js
+++ nodeJs/globals.js
@@ -1,2 +1,17 @@
-const _=require('lodash')
+const EventEmitter= require('events')
 
+const customEmitter = new EventEmitter()
+
+customEmitter.on('response', ()=>{
+    console.log('data received')
+})
+
+customEmitter.on('response', ()=>{
+    console.log('data received')
+})
+
+customEmitter.on('r2', ()=>{
+    console.log('data received')
+})
+
+customEmitter.emit('response')
