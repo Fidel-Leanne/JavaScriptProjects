e11535ccd09479b370f8bf056054a83ca7cb7a1a
fidelia <fidelialtm13@gmail.com>
2023-08-31 19:53:10 +0200

    js26

--- nodeJs/globals.js
+++ nodeJs/globals.js
@@ -1,17 +1,26 @@
-const EventEmitter= require('events')
+// const EventEmitter= require('events')
 
-const customEmitter = new EventEmitter()
+// const customEmitter = new EventEmitter()
 
-customEmitter.on('response', ()=>{
-    console.log('data received')
+// customEmitter.on('response', ()=>{
+//     console.log('data received')
+// })
+
+// customEmitter.on('response', ()=>{
+//     console.log('data received')
+// })
+
+// customEmitter.on('r2', ()=>{
+//     console.log('data received')
+// })
+
+// customEmitter.emit('response')
+
+const {createReadStream, createWriteStream}= require('fs')
+
+const stream = createReadStream('./content/first.txt')
+const writeStream= createWriteStream('./content/new.txt')
+
+stream.on('data',(result)=>{
+    console.log(`Data: ${result}`)  
 })
-
-customEmitter.on('response', ()=>{
-    console.log('data received')
-})
-
-customEmitter.on('r2', ()=>{
-    console.log('data received')
-})
-
-customEmitter.emit('response')
