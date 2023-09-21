e5ff8d08852cf0a9093c3e2486c72a997d30e3e7
fidelia <fidelialtm13@gmail.com>
2023-09-21 19:55:40 +0200

    mongo initialisation 2.0

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -1,52 +1,52 @@
-const { readFile, writeFile } = require('fs');
-const http = require('http');
-const util= require('util')
+// const { readFile, writeFile } = require('fs');
+// const http = require('http');
+// const util= require('util')
 
-const readFilePromise= util.promisify(readFile)
-const writeFilePromise= util.promisify(writeFile)
+// const readFilePromise= util.promisify(readFile)
+// const writeFilePromise= util.promisify(writeFile)
 
-// Create a new file
-const filename = './content/first.txt';
-// writeFile(filename, 'hello its fidel', err => {
-//     if (err) {
-//         console.error('Error writing to file:', err);
-//         return;
+// // Create a new file
+// const filename = './content/first.txt';
+// // writeFile(filename, 'hello its fidel', err => {
+// //     if (err) {
+// //         console.error('Error writing to file:', err);
+// //         return;
+// //     }
+// //     console.log(`File ${filename} has been created!`);
+// // });
+
+// // const getText = (path) => {
+// //     return new Promise((resolve, reject) => {
+// //         readFile(path, 'utf-8', (err, data) => {
+// //             if (err) {
+// //                 reject(err);
+// //             } else {
+// //                 resolve(data);
+// //             }
+// //         });
+// //     });
+// // };
+
+
+// // getText('./content/first.txt')
+// // .then ((result)=>console.log(result))
+// // .catch((err)=>console.log(err))
+
+// const server = http.createServer(async (req, res) => {
+//     console.log('request event');
+//     try {
+//        const first= await readFilePromise('./content/first.txt','utf-8' )
+//        const second=await readFilePromise('./content/second.txt','utf-8')
+
+//        console.log(first)
+//        res.end(`${first} ${second}`)
+        
+//     } catch (err) {
+//         res.end('Error reading file.');
 //     }
-//     console.log(`File ${filename} has been created!`);
 // });
 
-// const getText = (path) => {
-//     return new Promise((resolve, reject) => {
-//         readFile(path, 'utf-8', (err, data) => {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(data);
-//             }
-//         });
-//     });
-// };
-
-
-// getText('./content/first.txt')
-// .then ((result)=>console.log(result))
-// .catch((err)=>console.log(err))
-
-const server = http.createServer(async (req, res) => {
-    console.log('request event');
-    try {
-       const first= await readFilePromise('./content/first.txt','utf-8' )
-       const second=await readFilePromise('./content/second.txt','utf-8')
-
-       console.log(first)
-       res.end(`${first} ${second}`)
-        
-    } catch (err) {
-        res.end('Error reading file.');
-    }
-});
-
-const port = 5000;
-server.listen(port, () => {
-    console.log(`server is listening on port ${port}`);
-});
+// const port = 5000;
+// server.listen(port, () => {
+//     console.log(`server is listening on port ${port}`);
+// });
